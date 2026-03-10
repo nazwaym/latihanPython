c6f8031d895f740fc499a7334c83cf763f75928b
nazwa yulianti <yuliantimunjanan@gmail.com>
2026-03-10 23:13:59 +0700

    upload latihan python pertemuan 2

---
+++ HelloIO.py
@@ -0,0 +1,14 @@
+"""
+Program dibawah ini dibuat oleh saya sendiri
+Program untuk menampilkan input dan output
+"""
+
+# Input dari pengguna
+nama = input("Masukan Nama Anda: ")
+nim = input("Masukan NIM Anda: ")
+jurusan = input("Masukan Jurusan Anda: ")
+
+# Output
+print("Nama :", nama)
+print("NIM :", nim)
+print("Jurusan :", jurusan)
---
+++ LoadFileData.py
@@ -0,0 +1,5 @@
+# Using "with" statement to read from the file
+with open('tips.csv', 'r') as file:
+    content = file.read()
+
+print(content)
